1dbb9309cc441fd83027d5f8133a9770469e4d4f
Cory Martin <murribu@gmail.com>
2024-09-04 15:56:41 -0500

    remove config cruft

--- config.d.ts
+++ config.d.ts
@@ -1,7 +1,5 @@
 export declare const config: {
-  certificateArn: string;
   repoOwner: string;
   repo: string;
   connectionArn: string;
-  domain: string;
 };
--- config.example.ts
+++ config.example.ts
@@ -1,10 +1,6 @@
 export const config = {
-  certificateArn:
-    "arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012",
   repoOwner: "murribu",
   repo: "wiki",
   connectionArn:
     "arn:aws:codestar-connections:us-east-1:123456789012:connection/12345678-1234-1234-1234-123456789012",
-  domain: "corymartin.click",
-  account: "123456789012",
 };
